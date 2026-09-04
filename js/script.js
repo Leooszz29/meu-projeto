0e76e744f9e86a2be469d4b36f978a02bd74e881
Leooszz29 <leo_pacheco29@hotmail.com>
2026-09-04 11:10:27 -0300

    Add station selection functionality to script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-// JavaScript do FitZone
+const stations = document.querySelectorAll('.station');
+
+stations.forEach(st => {
+  st.addEventListener('click', () => {
+    stations.forEach(s => s.classList.remove('selected'));
+    st.classList.add('selected');
+  });
+});
+
+function escolherGenero(genero) {
+  localStorage.setItem('generoFitZone', genero);
+  window.location.href = 'login.html';
+}
