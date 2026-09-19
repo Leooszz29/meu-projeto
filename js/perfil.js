8f5e199db5618ae309d1eb264e0fb806a1a30700
Leooszz29 <leo_pacheco29@hotmail.com>
2026-09-19 10:38:09 -0300

    Update delete button to use SVG icon
    Replaced delete button text with an SVG icon for better visual representation.

--- js/perfil.js
+++ js/perfil.js
@@ -873,7 +873,18 @@ deleteButton.type = 'button';
 deleteButton.className =
     'history-delete-btn';
 
-deleteButton.innerHTML = '🗑';
+deleteButton.innerHTML = `
+    <svg
+        viewBox="0 0 24 24"
+        aria-hidden="true"
+    >
+        <path d="M3 6h18" />
+        <path d="M8 6V4h8v2" />
+        <path d="M19 6l-1 14H6L5 6" />
+        <path d="M10 10v6" />
+        <path d="M14 10v6" />
+    </svg>
+`;
 
 deleteButton.title =
     'Excluir este registro';
